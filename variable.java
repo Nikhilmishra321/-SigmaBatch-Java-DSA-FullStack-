9386925c25a60d316ed0af9ee53724aea973e4de
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-12-30 10:54:12 +0530

    variable of java

---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        //variable are container to store the value
+
+        //string
+        String name="Tony Stark";
+        System.out.println(name);
+    }
+    
+}
