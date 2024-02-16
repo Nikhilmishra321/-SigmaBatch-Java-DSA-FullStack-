44dd62d6ed4cab7c848077e84cfba5cc12799a58
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-16 16:22:57 +0530

    data type assiging

---
+++ Variable&Datatype/Data-type-java/data_type.java
@@ -0,0 +1,37 @@
+
+/*   Data type in java
+
+ *  1.Primitive datatype     
+ * >byte  1 bytes
+ * >short 2 bytes
+ * >char  2 bytes
+ * >boolean  1 bytes
+ * >int  4 bytes
+ * >float 4 bytes
+ * >double 8 bytes
+ * >long 8 bytes
+ * 
+ * 2. Non-primitive datatype
+ * >String
+ * >Arrays
+ * >Class
+ * >Object
+ * >Interface
+ *  
+ */
+public class data_type {
+    public static void main(String[] args) {
+        byte b=5;
+        System.out.println(b);
+        char c='a';
+        System.out.println(c);
+        boolean var =false;
+        System.out.println(var);
+        int num=25;
+        System.out.println(num);
+
+
+        
+    }
+    
+}
