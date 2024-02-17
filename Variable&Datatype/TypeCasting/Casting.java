df3d077c994ec4e61e96b35357807762802c3915
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-17 21:42:43 +0530

    type casting

---
+++ Variable&Datatype/TypeCasting/Casting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Casting {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        float a =22.13f;
+        int b = (int)a;
+        System.out.println(b);
+        sc.close();
+
+
+    }
+    
+}
---
+++ Variable&Datatype/TypeConversion/Conversion.java
@@ -0,0 +1,22 @@
+//conversion happen 
+//a. type comptaible
+//b. destination type > source type
+
+//byte-> short-> int->float->long->double
+
+import java.util.Scanner;
+
+public class Conversion {
+    public static void main(String[] args) {
+        // int a=20;
+        // float b=a;
+        // System.out.println(b);
+
+        Scanner sc = new Scanner(System.in);
+        float number =sc.nextInt();
+        System.out.println(number);
+        sc.close();
+        
+    }
+    
+}
