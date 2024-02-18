458497818d1be7a0a95f17d5eae51631438fbdfb
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-18 11:41:18 +0530

    logical operator

---
+++ Operators/Logical_Operator/Logic.java
@@ -0,0 +1,14 @@
+package Operators.Logical_Operator;
+/* and && both statement is true than statement is true
+ * or ||  both statement are false than final answer is false otherwise answer is ture
+ * not !
+ */
+
+public class Logic {
+    public static void main(String[] args) {
+        // System.out.println((5>3) && (3<5)); and operator
+        // System.out.println((4>3) || (3>4));
+        
+    }
+    
+}
