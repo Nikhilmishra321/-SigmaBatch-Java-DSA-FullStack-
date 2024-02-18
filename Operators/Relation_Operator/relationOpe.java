9fa6bdbd54255697647a615cc5b1a7891a568416
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-18 11:23:10 +0530

    realation operator

---
+++ Operators/Relation_Operator/relationOpe.java
@@ -0,0 +1,18 @@
+package Operators.Relation_Operator;
+/* ==
+ * !=
+ * <,>
+ * >=,<=
+ */
+public class relationOpe {
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a <b);
+        System.out.println(a > b);
+        
+    }
+    
+}
