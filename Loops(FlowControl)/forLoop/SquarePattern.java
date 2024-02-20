7ba5cc884fbce028bb5fbe9eb0af89f46525382c
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-20 16:01:01 +0530

    for loop using pattern

---
+++ Loops(FlowControl)/forLoop/SquarePattern.java
@@ -0,0 +1,10 @@
+
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        for(int loop=1; loop<4; loop++){
+            System.out.println("****");
+        }
+    }
+    
+}
