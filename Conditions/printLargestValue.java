ee555ab9464a97b786c297f331fae294ba0ee067
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-20 10:24:44 +0530

    largest of 2 Number

--- Conditions/printLargestValue.java
+++ Conditions/printLargestValue.java
@@ -2,7 +2,13 @@ package Conditions;
 
 public class printLargestValue {
     public static void main(String[] args) {
-        
+        int A=1 , B=5;
+
+        if(A>=B){
+            System.out.println("A");
+        }else{
+            System.out.println("B");
+        }
     }
     
 }
