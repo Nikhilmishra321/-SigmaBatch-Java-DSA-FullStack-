065950881039ea7a7c525dd22f9f5874ff449ef8
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-23 10:24:42 +0530

    product of two number

---
+++ Function&Method/Product_a_b.java
@@ -0,0 +1,17 @@
+
+
+public class Product_a_b {
+
+    public static int multiply(int a, int b){
+        int product=a*b;
+        return product;
+
+
+    }
+    public static void main(String[] args) {
+       int prod= multiply(10, 30);
+       System.out.println(prod);
+        
+    }
+    
+}
