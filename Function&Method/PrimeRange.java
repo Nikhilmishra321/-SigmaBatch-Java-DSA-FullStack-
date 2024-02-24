7240df1bf3aa0d3b51b1c3444cf5e3f397d04e53
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-24 09:11:15 +0530

    decimal to binary

---
+++ Function&Method/PrimeRange.java
@@ -0,0 +1,15 @@
+
+
+public class PrimeRange {
+
+    public static void primeInRange(int n){
+        for(int i=2; i<n; i++){
+            
+        }
+
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
