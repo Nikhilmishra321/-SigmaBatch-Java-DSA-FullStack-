8fcccf42be8c897d25850d9913a4ef9a6d9beb1d
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-02-26 17:52:47 +0530

    calculate percentage

--- Arrays(Part_1)/ArraysCC.java
+++ Arrays(Part_1)/ArraysCC.java
@@ -15,6 +15,10 @@ public class ArraysCC {
         System.out.println("Maths:" + marks[2]);
 
 
+        int percentage=(marks[0] + marks[1] + marks[2]) / 3;
+
+        System.out.println("Percentage " + percentage);
+
 
     }
     
